8b63fdfbe7b1926ba909335635ad141e6ebddeb6
42silasweis <silasweis18@gmail.com>
2019-02-04 08:47:05 -0800

    AA

---
+++ 42silasweisgit.sh
@@ -0,0 +1,6 @@
+git config --list;
+git config --global user.username  42silasweis;
+git config --global user.name  42silasweis;
+git config --global user.user 42silasweis;
+git config --global user.email silasweis18@gmail.com;
+git config --list;
